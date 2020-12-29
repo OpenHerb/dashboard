7e4494877de702bb519b883f4b6eefcc849d78d9
cSDes1gn <christian911@sympatico.ca>
2020-12-29 01:13:32 -0500

    gauge adjustments

--- app/static/gauge.js
+++ app/static/gauge.js
@@ -23,19 +23,19 @@ var tempGauge = Gauge(
 var humidityGauge = Gauge(
     document.getElementById("humidity-gauge"), {
         min: 0,
-        max: 100,
+        max: 50,
         value: 50,
         radius:100,
         showValue: true,
         color: function(value) {
-            if(value < 25) {
-            return "#5ee432";
-            }else if(value < 50) {
-            return "#fffa50";
-            }else if(value < 75) {
-            return "#f7aa38";
+            if(value >= 20 && value <= 35) {
+                return "#5ee432";
+            }else if(value >= 15 && value <= 40) {
+                return "#fffa50";
+            }else if(value >= 10 && value <= 45) {
+                return "#f7aa38";
             }else {
-            return "#ef4655";
+                return "#ef4655";
             }
         }
     }
@@ -69,11 +69,11 @@ asGauge.setValue(50);
 
 (function loop() {
     var randTemp = (Math.random() * 30)+10;
-    var randHumidity = Math.random() * 100;
+    var randHumidity = Math.random() * 50;
     var randAs = Math.random() * 100;
     // setValueAnimated(value, durationInSecs);
     tempGauge.setValueAnimated(randTemp, 2);
-    humidityGauge.setValueAnimated(100 - randHumidity, 2);
+    humidityGauge.setValueAnimated(randHumidity, 2);
     asGauge.setValueAnimated(100 - randAs, 2);
     window.setTimeout(loop, 6000);
 })();
