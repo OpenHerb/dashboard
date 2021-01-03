1f280bb66982fbb625be0a4d233cbf10f7f2e382
cSDes1gn <christian911@sympatico.ca>
2021-01-03 00:18:14 -0500

    connecting firebase telemetry to gauges

--- app/static/gauge.js
+++ app/static/gauge.js
@@ -1,4 +1,40 @@
 
+// default values for telemetry
+var temp = null
+var humidity = null
+var lux = null
+var injection = null
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDL9CwpORKoxZkAbmg47zcLZrExAvPNH6c",
+    authDomain: "openherb-89fa0.firebaseapp.com",
+    databaseURL: "https://openherb-89fa0-default-rtdb.firebaseio.com",
+    projectId: "openherb-89fa0",
+    storageBucket: "openherb-89fa0.appspot.com",
+    messagingSenderId: "347898545625",
+    appId: "1:347898545625:web:7c459f88074b52e4e805c8",
+    measurementId: "G-B0ZMNQWBT0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+// Get a reference to the database service telemetry topic
+var telemetry = firebase.database().ref('/');
+// only call async listener with most recent push
+telemetry.on('child_added', (snapshot) => {
+    const data = snapshot.val();
+    temp = data.T
+    humidity = data.H
+    lux = data.L
+    injection = data.I
+    console.log("Temperature: " + temp + " °C");
+    console.log("Soil Humidity: " + humidity + " %");
+    console.log("Luminous Flux: " + lux + " Lux");
+    console.log("Last Injection: " + injection);
+});
+
 var tempGauge = Gauge(
     document.getElementById("temp-gauge"), {
         min: 10,
@@ -68,12 +104,9 @@ humidityGauge.setValue(25);
 luxGauge.setValue(50);
 
 (function loop() {
-    var randTemp = (Math.random() * 30)+10;
-    var randHumidity = Math.random() * 50;
-    var randLux = Math.random() * 100;
     // setValueAnimated(value, durationInSecs);
-    tempGauge.setValueAnimated(randTemp, 2);
-    humidityGauge.setValueAnimated(randHumidity, 2);
-    luxGauge.setValueAnimated(100 - randLux, 2);
-    window.setTimeout(loop, 2000);
+    tempGauge.setValueAnimated(temp, 2);
+    humidityGauge.setValueAnimated(humidity, 2);
+    luxGauge.setValueAnimated(lux, 2);
+    window.setTimeout(loop, 1000);
 })();
