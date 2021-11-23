b66c5e4378a437b433d460dc42d6837c4d502c5f
TravJav <travishaycock92@gmail.com>
2021-11-22 21:42:57 -0500

    Added temperature gauge that will contain and display information in context of the temperature sensor data

--- open-herb-dashboard/src/app/temperature-gauge/temperature-gauge.component.ts
+++ open-herb-dashboard/src/app/temperature-gauge/temperature-gauge.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temperature-gauge.component.css']
 })
 export class TemperatureGaugeComponent implements OnInit {
+  // single: Array<any>[];
+  maxTemperature: number;
+  minTemperature: number;
+  legend: boolean = false;
+  legendPosition: string = 'below';
 
-  constructor() { }
+  colorScheme = {
+    domain: ['#7aa3e5']
+  };
 
-  ngOnInit() {
+  constructor() { 
+    Object.assign(this, { single });
+    this.maxTemperature = 40;
+    this.minTemperature = 0;
+  }
+
+  public ngOnInit(): void {
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
+
+
+export var single = [
+  {
+    "name": "Temperature",
+    "value": 40
+  }
+];
