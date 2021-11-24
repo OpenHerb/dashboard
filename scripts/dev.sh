3c611ba3125fc1bbec0a283dacadf3916acbc24e
cSDes1gn <christian911@sympatico.ca>
2021-11-23 23:15:27 -0500

    add builder script

---
+++ scripts/dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+source .env
+
+printf "%b" "${OKB}Authenticating $USERNAME with github container registry${NC}\n"
+echo "$CR_PAT" | docker login ghcr.io -u "$USERNAME" --password-stdin
+printf "%b" "${OKB}Building project${NC}\n"
+docker compose up
